b822188c744880e91544a934557f957c6397987f
zhikaiong2001 <ongzk2001@gmail.com>
2023-05-24 02:09:34 +0800

    Soul system,soul bar
    Added soul system, soul bar.

--- My project/Assets/Scripts/Health.cs
+++ My project/Assets/Scripts/Health.cs
@@ -38,6 +38,11 @@ public class Health : MonoBehaviour
         }
     }
 
-    
+    public void Heal(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, startingHealth);
+    }
+
+
 
 }
--- My project/Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ My project/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -11,6 +11,8 @@ public class PlayerAttack : MonoBehaviour
     public float attackRange = 0.5f;
     public int attackDamage = 40;
     public LayerMask enemyLayers;
+    public Soul souls;
+    public float soulsPerAttack;
 
     public float attackRate = 2f;
     private float nextAttackTime = 0f;
@@ -48,7 +50,8 @@ public class PlayerAttack : MonoBehaviour
 
         foreach(Collider2D enemy in hitEnemies)
         {
-            enemy.GetComponent<Enemy>().TakeDamage(attackDamage); 
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            souls.GainSoul(soulsPerAttack);
         }
         
     }

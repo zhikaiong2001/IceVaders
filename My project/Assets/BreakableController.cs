ddb087d8c531a4ec585f9038dcd06517acacd8ff
Nauseous-Spartan <99877894+Nauseous-Spartan@users.noreply.github.com>
2023-07-21 15:15:12 +0800

    Finished all rooms

---
+++ My project/Assets/BreakableController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BreakableController : MonoBehaviour
+{
+    [Header("Breakable Objects")]
+    public static bool startWall1;
+
+    void Start()
+    {
+
+    }
+}

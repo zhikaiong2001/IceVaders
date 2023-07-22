1fb3544188e83d483bbe0670ea3d1e6992c50424
zhikaiong2001 <ongzk2001@gmail.com>
2023-07-22 19:13:30 +0800

    Added bgm music that transitions between scenes, added boss music, added take dmg and death sound fx for goblin

--- My project/Assets/DoNotDestroy.cs
+++ My project/Assets/DoNotDestroy.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class DoNotDestroy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
+        if(this.gameObject.tag == "Game Music")
+        {
+            GameObject[] musicObj = GameObject.FindGameObjectsWithTag("Game Music");
+            if (musicObj.Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+            
+            
+        }
+        if (this.gameObject.tag == "Boss Music")
+        {
+            GameObject[] bossObj = GameObject.FindGameObjectsWithTag("Boss Music");
+            if (bossObj.Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+
+
+        }
+        DontDestroyOnLoad(this.gameObject);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (this.gameObject.name == "BGM Manager")
+        {
+            return;
+        }
+        GameObject[] golem = GameObject.FindGameObjectsWithTag("Golem");
+        GameObject[] fireApostle = GameObject.FindGameObjectsWithTag("FireApostle");
+        if (golem.Length > 0 || fireApostle.Length > 0)
+        {
+            if (this.gameObject.tag == "Game Music")
+            {
+                this.gameObject.GetComponent<AudioSource>().enabled = false;
+            }
+            else
+            {
+                this.gameObject.GetComponent<AudioSource>().enabled = true;
+            }
+
+
+        }
+        else
+        {
+            if (this.gameObject.tag == "Boss Music")
+            {
+                this.gameObject.GetComponent<AudioSource>().enabled = false;
+            }
+            else
+            {
+                this.gameObject.GetComponent<AudioSource>().enabled = true;
+            }
+        }
     }
 }
+
+    

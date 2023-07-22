52500f98b79b4164d939ac048f3dba5b717e60cd
Nauseous-Spartan <99877894+Nauseous-Spartan@users.noreply.github.com>
2023-07-23 00:52:31 +0800

    Added Enemies to most scenes

---
+++ My project/Assets/Sword.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            gameObject.SetActive(false);
+            Player.attackDamage += 10;
+        }
+    }
+}
